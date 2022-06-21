79a44049a812cc378e6cb01e5c4dff479cf71627
jonrato <jonatas_ferreira@usp.br>
2022-06-21 14:59:50 -0300

    cases & date

--- src/assets/Chart.js
+++ src/assets/Chart.js
@@ -3,3 +3,21 @@ import api from './API';
 import { useEffect, useState } from "react";
 import { Line } from "react-chartjs-2";
 import './chart.css';
+
+export default function ChartTest(){
+    const [dataChart, setDataChart] = useState({});
+
+    useEffect(() => {
+        const fetchData = async () => {
+            await api.get('dayone/country/brazil/status/confirmed')
+            .then(response => {
+                for(dataObj of response.data){
+                    confirmedCases.push(parseInt(dataObj.Cases));
+                    let tempDate = new Date(dataObj.Date);
+                    dateOfCases.push(tempDate.getUTCDate());
+                }
+            });
+        }
+    },[])
+}
+
