36bc6256fa4fffb3204ca05888f50aa92502fa43
jonrato <jonatas_ferreira@usp.br>
2022-06-21 15:27:58 -0300

    Chart

--- src/assets/Chart.js
+++ src/assets/Chart.js
@@ -4,20 +4,33 @@ import { useEffect, useState } from "react";
 import { Line } from "react-chartjs-2";
 import './chart.css';
 
-export default function ChartTest(){
-    const [dataChart, setDataChart] = useState({});
-
-    useEffect(() => {
-        const fetchData = async () => {
-            await api.get('dayone/country/brazil/status/confirmed')
-            .then(response => {
-                for(dataObj of response.data){
-                    confirmedCases.push(parseInt(dataObj.Cases));
-                    let tempDate = new Date(dataObj.Date);
-                    dateOfCases.push(tempDate.getUTCDate());
-                }
-            });
-        }
-    },[])
-}
-
+export default function ChartTest() {
+    const [ dataChart, setDataChart ] = useState ( {} );
+  useEffect(() => {
+      const fetchData = async () => {
+        let confirmedCases = [];
+        let dateOfCases = [];
+        await api.get('dayone/country/brazil/status/confirmed')
+          .then ( response => {
+            for ( let dataObj of response.data ) {
+              confirmedCases.push(parseInt(dataObj.Cases));
+              let tempDate = new Date (dataObj.Date);
+              dateOfCases.push(tempDate.getUTCDate());
+            }
+        }); 
+        setDataChart({ 
+          labels: dateOfCases, 
+          datasets: [{ 
+            label: 'Confirmed cases', 
+            data: confirmedCases 
+          }]
+        });
+      fetchData();
+    }
+    }, []);
+    return( 
+      
+        <Line data={ dataChart }/>
+      
+    )
+  }
